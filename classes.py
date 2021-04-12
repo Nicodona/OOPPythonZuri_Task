274e8d9ab4fc6b839bca696baa9671208f7af8a6
njei nicodona <njeinicodona@gmail.com>
2021-04-12 21:10:26 +0100

    First trial of class and object task added option

--- classes.py
+++ classes.py
@@ -20,32 +20,22 @@ class Budget:
         self.amount = self.amount - transfer
         return transfer
 
-    def transferFrom(self):
-
-        recieved = food.transferTo()
-        print(recieved)
-        # self.amount = self.amount + recieved
-        # return self.amount
-
-
-
-
 
     def computeBalance(self):
-        print('this is the account details of categories')
-        print('accountBalance for food category is: {}'.format(food.amount))
-        print('accountBalance for clothing category is: {}'.format(clothing.amount))
-        print('accountBalance for entertainment category is: {}'.format(entertaintment.amount))
+        print('this is the account details of categories\n')
+        print('accountBalance for food category is: {}\n'.format(food.amount))
+        print('accountBalance for clothing category is: {}\n'.format(clothing.amount))
+        print('accountBalance for entertainment category is: {}\n\n'.format(entertaintment.amount))
         acc = food.amount+clothing.amount+entertaintment.amount
         return acc
 
-
+# objects or instance of the class Budget
 food = Budget('food', 20000)
 clothing = Budget('clothing', 10000)
 entertaintment = Budget('entertainment', 5000)
 ac = Budget('', '' )
 
-
+#codes to test the class by calling class methods and attributes and also other manipulations
 print('what do you want to do with a category')
 choice = int(input('enter choice to either\n 1 to deposite\n or 2 to withdraw\n or 3 to trannsfer fundfrom one category to another\n4 to chect account details'))
 if choice == 1:
@@ -78,9 +68,9 @@ elif choice == 2:
 elif choice == 3:
     obj = int(input( 'what category do you want to work with?\n enter 1 for food category\n enter 2 for clothing category\n enter 3 for entertainments category\n'))
     if obj == 1:
+        food.transferTo()
         print(' enter 1 to transfer to clothing category ')
         print(' enter 2 to transfer to entertainment category ')
-        food.transferTo()
         print(food.amount)
         trans = int(input('which category are you traffering cash to?'))
         if trans == 1:
@@ -95,9 +85,10 @@ elif choice == 3:
 
 
     elif obj == 2:
+        clothing.transferTo()
         print(' enter 1 to transfer to food category ')
         print(' enter 2 to transfer to entertainment category ')
-        clothing.transferTo()
+
         print(clothing.amount)
 
 
@@ -114,9 +105,10 @@ elif choice == 3:
 
 
     elif obj == 3:
+        entertaintment.transferFrom()
         print(' enter 1 to transfer to food category ')
         print(' enter 2 to transfer to clothing category ')
-        entertaintment.transferFrom()
+
         print(entertaintment.amount)
         trans = int(input('which category are you traffering cash to?'))
 
